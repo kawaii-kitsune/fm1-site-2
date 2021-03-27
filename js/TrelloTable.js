8b434850de4ed0be72efb14ef603e1e742e40d05
kawaii-kitsune <72494172+kawaii-kitsune@users.noreply.github.com>
2021-03-27 17:51:20 +0200

    DID SOMETHING BUT I UNDID IT

--- js/TrelloTable.js
+++ js/TrelloTable.js
@@ -3,9 +3,9 @@ let secret='1b615199157b6602dd8b0fc83c744fd8d92d415bc0840435441e5746b7da7dbf'
 let id='605e590e2698ff32b32f98ed'
 let token='cf15126dd5323a15aa473bc317c6e6b75c460169b8d90d9703883e2f10879ced'
 let data;
-window.onload = function(){
-    getInfo();
-};
+// window.onload = function(){
+//     getInfo();
+// };
 function getInfo(){
     fetch('https://api.trello.com/1/boards/'+id+'/cards?key='+key+'&token='+token, {
   method: 'GET'
